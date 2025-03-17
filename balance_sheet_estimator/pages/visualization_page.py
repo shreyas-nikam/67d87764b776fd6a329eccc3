b1f6273fdf89601601f52db0a577ee9f8a117a03
Shreyas Nikam <158344330+shreyas-nikam@users.noreply.github.com>
2025-03-17 16:49:52 -0400

    Add balance_sheet_estimator/pages/visualization_page.py

---
+++ balance_sheet_estimator/pages/visualization_page.py
@@ -0,0 +1,6 @@
+import streamlit as st
+import plotly.graph_objects as go
+
+def visualization_page(total_financial_wealth, estimated_human_capital, estimated_liabilities, estimated_net_worth):
+    ...
+    # The rest of the visualization page code from before
